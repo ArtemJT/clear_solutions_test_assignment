d120ae4bf22caaad7540531a767f854b61279d7a
Artem <105941754+ArtemJT@users.noreply.github.com>
2024-05-04 16:00:00 +0300

    Initial commit

---
+++ src/test/java/com/example/clear_solutions_test_assignment/ClearSolutionsTestAssignmentApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.clear_solutions_test_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionsTestAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+        ClearSolutionsTestAssignmentApplication.main(new String[]{});
+    }
+
+}
